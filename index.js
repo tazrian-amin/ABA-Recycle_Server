8c8d8795694f256ae0052bf315e3b5c173211264
tazrian-amin <amin.tazrian1979@gmail.com>
2022-11-30 18:44:31 +0600

    added middlewire to server and connected it to mongodb

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+// setting middleware
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.nl4sfa8.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try {
+        const usersCollection = client.db('ABA-recycle').collection('users');
+    }
+    finally {
+
+    }
+}
+
+run().catch(err => console.error(err));
+
+app.get('/', async (req, res) => {
+    res.send('ABA-recycle server running');
+})
+
+app.listen(port, () => console.log(`ABA-recycle server running on port: ${port}`));
